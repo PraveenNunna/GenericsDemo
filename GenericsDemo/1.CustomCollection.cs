44a09210f536b0c317ee2eeb4acc6f031984d1cf
Praveen Nunna <praveen.nunna@hotmail.com>
2018-10-10 21:15:20 +0200

    Added first example

---
+++ GenericsDemo/1.CustomCollection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace GenericsDemo
+{
+    public class CustomCollection<T>
+    {
+        public CustomCollection()
+        {
+            Collection = new ArrayList();
+        }
+        public ArrayList Collection { get; set; }
+        public int Add(T item)
+        {
+            Collection.Add(item);
+            return Collection.Count;
+        }
+    }
+
+    public class UseExample1
+    {
+        public void AddItems()
+        {
+            var intList = new List<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+
+            var stringList = new CustomCollection<string>();
+
+            stringList.Add("Praveen");
+            stringList.Add("Nunna");
+
+            var employeeList = new CustomCollection<Employee>();
+
+            employeeList.Add(new Employee() { Id = 1, Name = "Employee1" });
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
