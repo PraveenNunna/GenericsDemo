92f3f7031a82264dc7ee98f2cd4b99b90f280956
Praveen Nunna <praveen.nunna@hotmail.com>
2018-10-16 14:03:20 +0200

    add some description

--- GenericsDemo/2.GenericList.cs
+++ GenericsDemo/2.GenericList.cs
@@ -84,3 +84,8 @@ namespace GenericsDemo
         }
     }
 }
+
+// We can apply constraints on type T. We can specify whether we need a class, struct or a base class to be the type. So that, we can get intelligence on the type mentioned with in the Generic class.
+
+// For example, in the above example, we can say  GenericList<T> : IEnumerable<T>, IGenericList<T> where T: IEmployee. only implementations of IEmployee can be created using this generic list.
+// So we can use methods available in IEmployee in the generic class.
