30e956ae790ace4713329a614af49b2cd1dfc064
Praveen Nunna <praveen.nunna@hotmail.com>
2018-10-30 14:18:22 +0100

    Added unittest project.

---
+++ CollectionsTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CollectionsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestListAdd()
+        {
+            //Arrange
+            List<int> list = new List<int>();
+
+            //act
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            //Assert
+            Assert.AreEqual(list.Count, 6);
+        }
+    }
+}
--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -8,8 +8,12 @@ namespace GenericsDemo
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            log.DebugFormat("fdsfdsfsf {0}", "Hi");
+            Console.WriteLine();
             new UsingGenericList().AddItemToList();
             Console.ReadKey();
         }
